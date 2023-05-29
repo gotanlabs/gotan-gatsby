b83c125feba65d4fff192325a3fbd54f962551f8
sofia.lagoa <sofia.lagoa@bitlogic.io>
2023-05-29 17:01:06 -0300

    Update style header

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./CustomLink.scss"
 
 
-function CustomLink({href, icon, style, text}) {
+function CustomLink({ href, icon, style, text }) {
 
   return (
     <a href={href} title={text} className={`custom-link ${style}`}>
-      {icon && icon}
-      {style !== "icon" && <p className="mb-0">{text}</p>}
-      
+      <p className="mb-0">{text}</p>
     </a>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,6 @@ import React from "react";
 import SanityImage from "gatsby-plugin-sanity-image";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import { MaterialIcon, FontAwesomeIcon} from "..";
-import { useTheme } from "../../context/themeContext";
 
 import "./Header.scss";
 import LinkBlock from "./LinkBlock";
@@ -12,33 +10,17 @@ import useHeader from "../../hooks/useHeader";
 
 const Header = () => {
   const data = useHeader().sanityHeader;
-  const { theme, toggleTheme } = useTheme();
-  
 
   return data !== null ? (
     <header className="header">
       <Navbar expand="lg">
         <Container>
           <Navbar.Brand href="/">
-            {theme === "dark" ? (
-                data.logo.imageDark !== null ? (
-              <SanityImage
-                {...data.logo.imageDark}
-                alt={`${data.logo.image.alt}`}
-                className="header__logo"
-              />
-              ) : (
-                <></>
-              )
-            ) :  data.logo.image.image !== null ? (
-              <SanityImage
-                {...data.logo.image.image}
-                alt={`${data.logo.image.alt}`}
-                className="header__logo"
-              />
-              ) : (
-                <></>
-            )}
+            <SanityImage
+              {...data.logo.image.image}
+              alt={`${data.logo.image.alt}`}
+              className="header__logo"
+            />
           </Navbar.Brand>
           {data.menu !== null ? <Menu links={data.menu.links} /> : <></>}
           {data.customLinkBlock !== null ? (
@@ -46,14 +28,6 @@ const Header = () => {
           ) : (
             <></>
           )}
-           <button onClick={toggleTheme} className="header__theme-toggle">
-        <div className={`moon ${theme === "dark" ? "" : "moon-animate"}`}>
-          <FontAwesomeIcon code="FaMoon" />
-        </div>
-      <div className={`sun ${theme === "dark" ? "sun-animate" : ""}`}>
-          <MaterialIcon code="MdWbSunny" />
-        </div>
-      </button>
         </Container>
       </Navbar>
     </header>
