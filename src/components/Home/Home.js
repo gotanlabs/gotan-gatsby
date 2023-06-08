aaa329c0e87834350e95e6819ea41fb00998908e
sofia.lagoa <sofia.lagoa@bitlogic.io>
2023-06-08 09:27:49 -0300

    Componente texto mas boton

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,23 @@
 import React from "react";
 import useHome from "../../hooks/useHome";
 import Banner from "../Banner/Banner";
-import {Seo, CustomSection} from "../";
+import {Seo, CustomSection, TextButton} from "../";
 import BannerSimple from "../BannerSimple/BannerSimple";
 import Logos from "../Logos/Logos";
 
 
 const Home = () => {
   const data = useHome().sanityHome;
+  console.log('home', data)
   return <>
     <Seo title='Home' description='' keywords='' />
     {data !== null ? <div>
       {data.banner !== null ? <Banner banner={data.banner} /> : <></>}
       {data.dinamicContent !== null ? <CustomSection sections={data.dinamicContent} /> : <></>}
-      <BannerSimple data={data.bannerSimple}/>
+      <TextButton data={data.textButton}/>
       <Logos logos={data.Logos} />
+      <BannerSimple data={data.bannerSimple}/>
     </div> : <div className="d-flex justify-content-center">
-     
     </div>}
   </>
 
--- src/components/TextButton/TextButton.js
+++ src/components/TextButton/TextButton.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { TextBlock } from "../";
-import './TextButton.scss'
+import './TextButton.scss';
+import { PortableText } from "@portabletext/react";
+import Icon from "../Icons/Icon";
+import CustomLink from "../CustomLink/CustomLink";
 
 
 const TextButton = ({ data }) => {
+    console.log('data', data)
     return (
-        <section className="container">
-            <div className="container-text">
-                <TextBlock
-                    key={data._key}
-                    title={data?.title}
-                    text={data._rawContent}
-                />
+        <>
+            <div className="background-textButton py-3 py-md-5">
+                <div className="container">
+                    <div className="textButton-text">
+                        <h1>{data.text.title}</h1>
+                        <PortableText value={data.text._rawContent} />
+                    </div>
+                    <div className="button-bannerSimple dark">
+                        <CustomLink
+                            href={data.iconButton.iconbutton.url}
+                            text={data.iconButton.iconbutton.text}
+                            icon={<Icon code={data.iconButton.icon.icon}></Icon>}
+                        />
+                    </div>
+                </div>
             </div>
-        </section>
+        </>
     );
 };
 
