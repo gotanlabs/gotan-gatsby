66703d9c6ad2d51b098cf04e5302e08980e9deac
Pilar <pilar.salto@bitlogic.io>
2023-06-14 11:27:25 -0300

    fix useAbout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,18 +2,17 @@
 import React from "react";
 import PropTypes from 'prop-types'
 import { Header, Footer} from "..";
-import ThemeProvider from "../../context/themeContext";
 
 import "./Layout.scss";
 
 const Layout = ({ children }) => {
 
   return (
-    <ThemeProvider>
+    <>
       <Header/>
       <main>{children}</main>
       <Footer/>
-    </ThemeProvider>
+    </>
   );
 };
 
