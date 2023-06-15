d7b19d1f512f972c41e930aeb6a3b8a4967342f7
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2023-06-15 13:45:13 -0300

    fix dual asimmetric

--- src/components/DualAsymmetric/DualAsymmetric.js
+++ src/components/DualAsymmetric/DualAsymmetric.js
@@ -1,35 +1,45 @@
-import SanityImage from "gatsby-plugin-sanity-image"
-import React from "react"
-import { PortableText } from "@portabletext/react"
-import "./DualAsymmetric.scss"
+import SanityImage from "gatsby-plugin-sanity-image";
+import React from "react";
+import { PortableText } from "@portabletext/react";
+import "./DualAsymmetric.scss";
 
 const DualAsymmetric = ({
-   
-  data: {
-    title,
-    image,
-    bannerStyle,
-    backgroundColor
-  },
+  data: { title, image, bannerStyle, backgroundColor },
 }) => {
-  
   return (
-    <div className={` DualAsymmetric py-3 py-md-5`} >
-    <div className={`container dualContainer ${bannerStyle == "asymmetricImageLeft" ? "flex-row-reverse" : ""}`} >
-      <div className={`titleContainer ${bannerStyle == "asymmetricImageRight" || bannerStyle == "asymmetricImageLeft" ? "col-lg-8 col-md-8 col-12" : "col-6"} ${backgroundColor}`}>
-        <h1 className="titleDual">{title}</h1>
-      </div >
-      {image && (
+    <div className={` DualAsymmetric py-3 py-md-5`}>
+      <div
+        className={`container dualContainer ${
+          bannerStyle == "asymmetricImageLeft" ? "flex-row-reverse" : ""
+        }`}
+      >
         <div
-          className={`asymmetricImage ${bannerStyle == "asymmetricImageRight" || bannerStyle == "asymmetricImageLeft" ? "col-3" :  "col-5"}`}   style={{ backgroundColor: "#14D3FC" }}
+          className={`titleContainer ${
+            bannerStyle == "asymmetricImageRight" ||
+            bannerStyle == "asymmetricImageLeft"
+              ? "col-lg-9 col-md-9 col-12"
+              : "col-6 me-2"
+          } ${backgroundColor}`}
         >
-          <SanityImage {...image.image} alt={image.alt} />
+          <h1 className="titleDual">{title}</h1>
         </div>
-      )}
-      <div  style={{ backgroundColor: "gray" }}></div>
+        {image && (
+          <div
+            className={`asymmetricImage ${
+              bannerStyle == "asymmetricImageRight" ||
+              bannerStyle == "asymmetricImageLeft"
+                ? "col-3"
+                : "col-6 ms-2"
+            } ${bannerStyle == "asymmetricImageRight" && "ms-3"} ${bannerStyle == "asymmetricImageLeft" && "me-3"}`}
+            style={{ backgroundColor: "#14D3FC" }}
+          >
+            <SanityImage {...image.image} alt={image.alt} />
+          </div>
+        )}
+        <div style={{ backgroundColor: "gray" }}></div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default DualAsymmetric
+export default DualAsymmetric;
