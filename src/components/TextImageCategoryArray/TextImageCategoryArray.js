5f0a921cf3418d3ffc9a6e685484f1780223f36e
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2023-06-23 11:19:15 -0300

    fix styles

--- src/components/TextImageCategoryArray/TextImageCategoryArray.js
+++ src/components/TextImageCategoryArray/TextImageCategoryArray.js
@@ -7,18 +7,18 @@ const TextImageCategoryArray = ({ sections }) => {
   
 
   return (
-    <div className="container section-padding ">
-    <div>
+    <div className="container section-padding textImageContainer">
+
       {sections.map((data, index) => (
         <div key={`data-${data.title}`} className={`textImage ${data.textRight}`}>
-       <div className={`textContainer ${data.textRight}Text col-lg-6 col-md-6 col-12`}>
+       <div className={`textContainer ${data.textRight}Text ${!data.category && 'textContainerCentered'} col-lg-6 col-md-6 col-12`}>
             {data.category && <p className={`textContainer__category ${(index % 3 === 0 ) ? 'firstColor' : index % 3 === 1 ? 'secondColor' : 'thirdColor'} 
            
             `}>{data.category}</p>}
-            {data.textBlock.title && <h3 className="textContainer__title">{data.textBlock.title}</h3>}
+            {data.textBlock.title && <h4 className="textContainer__title">{data.textBlock.title}</h4>}
             <PortableText value={data.textBlock._rawContent} />
           </div>
-          <div className={`picture ${data.textRight}Picture col-lg-6 col-md-6 col-12`}>
+          <div className={`picture ${data.textRight}Picture  ${!data.category && 'smallerPicture'}  col-lg-6 col-md-6 col-12`}>
            
            {data.image && <SanityImage
              {...data.image.image}
@@ -29,7 +29,7 @@ const TextImageCategoryArray = ({ sections }) => {
        </div>
         </div>
       ))}
-    </div>
+
   </div>
   );
 };
